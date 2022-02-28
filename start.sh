5d6cda1fcf6f8b19ab6e302b2539d8ed6b02709e
King Baron ™ <85443712+Alfa00006@users.noreply.github.com>
2022-02-28 11:41:32 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "Cloning Repo, Please Wait..."
+git@github.com:Alfa00006/Radios3.git /Radios3
+echo "Installing Requirements..."
+cd /Radios3
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 main.py
